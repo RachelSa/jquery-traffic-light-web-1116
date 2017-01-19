e26cd45222cd5175e89a44e66c348527d0a9ff81
github username <github email address>
2017-01-18 20:14:04 -0500

    done except blink red

--- script.js
+++ script.js
@@ -15,13 +15,53 @@ $(document).ready(function() {
    );
   
    /* code for button 3 -- Turn the light Yellow */
-  
+  $('#button3').click(
+     function() {
+         $('#middle-light').css("background", "yellow");
+     }
+   );
    /* code for button 4 -- Turn the light Green */
-  
+   $('#button4').click(
+     function() {
+         $('#bottom-light').css("background", "green");
+     }
+   );
    /* code for button 5 -- Say GO! */
+   $('#button5').click(
+     function() {
+         $('#bottom-light').html("<p> GO </p>");
+     }
+   );
 
    /* code for button 6 -- Make Green Light Blink -- Extension!! */
+  //var timer = setInterval(blink, 10000); 
+
+  //$('#button6').click(
+    
+     function blink() {
+         $('#top-light').css("background", "red");
+     }
+
+  // );
   
+  // setInterval(function() {
+
+  //   while (true) {
+  //     if (toggle == 0) {
+  //       $('#top-light').css("background", "red")
+  //       toggle == 1
+  //     }
+  //     else if (toggle == 1) {
+  //       $('#top-light').css("background", "white")
+  //       toggle == 0
+  //     }
+  //   }
+  // }, 1000);
+
+
+
+     
+   
 });
                   
                   
